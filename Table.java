c1f48ea19185f107bb6f1775667de565b71da904
wasantan23 <89757403+wasantan23@users.noreply.github.com>
2022-03-07 11:09:14 -0500

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,11 @@
+public class Table { //table class
+  int tableID;
+Person [] p1 = new Person [10]; //an array of 10 people
+
+
+public Table (int tableID) { //overloaded constructor that takes in the tableID - do we need a constructor? 
+  
+  } //closes construcotr 
+} //closes class
+
+
