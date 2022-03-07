c6d01b1d1b0e9234c535d8b1f8676a24c0670797
wasantan23 <89757403+wasantan23@users.noreply.github.com>
2022-03-07 11:07:35 -0500

    Create Banquet.java

---
+++ Banquet.java
@@ -0,0 +1,7 @@
+public class Banquet { 
+int numPpl;
+int [][] pplFromCo = new int [16] [20] //creates a 2D array - 16 rows, 20 colums 
+  Person [][] p2 = new Person [10][10] //how are we gonna remove a person - arraylist or 2d array?? - creates a 2D array - 10 rows, 10 colums
+  //ArrayList<String> companies  = new ArrayList<String>();
+  //ArrayList<ArrayList<String>> board = new ArrayList<ArrayList<String>>();
+} //closes the class
