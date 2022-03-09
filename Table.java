4b79de75e1b8ac85e95f7321a836753118954393
wasantan23 <89757403+wasantan23@users.noreply.github.com>
2022-03-09 11:06:27 -0500

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,11 +1,52 @@
 public class Table { //table class
   int tableID;
-Person [] p1 = new Person [10]; //an array of 10 people
+//  Person [] p1 = new Person [10]; //an array of 10 people
 
 
 public Table (int tableID) { //overloaded constructor that takes in the tableID - do we need a constructor? 
   
   } //closes construcotr 
+
+  public void Table1 () {
+
+  }
+
+  public void Table2 () {
+
+  }
+
+  public void Table3() {
+    
+  }
+
+  public void Table4() {
+    
+  }
+
+  public void Table5() {
+    
+  } 
+
+  public void Table6() {
+    
+  } 
+
+  public void Table7() {
+    
+  } 
+
+  public void Table8() {
+    
+  } 
+
+  public void Table9() {
+    
+  } 
+
+  public void Table10() {
+    
+  } 
+  
 } //closes class
 
 
